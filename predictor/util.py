441d1b609727b58abb8b0e285bc9550f221e5aed
Adel Hassan <sean.dashals@gmail.com>
2020-05-27 17:44:01 -0500

    Added Vector3 class to python util

--- predictor/util.py
+++ predictor/util.py
@@ -8,6 +8,33 @@ class Atom:
         self.y = y
         self.z = z
 
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __add__(self, other):
+        return Vector(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+        )
+    
+    def __sub__(self, other):
+        return Vector(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+        )
+    
+    def cross(self, other):
+        return Vector(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y - other.x,
+        )
+
 def get_charge(charge_str):
     if len(charge_str) != 2:
         raise Exception("Invalid charge string: " + charge_str + "|")
